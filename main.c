9983aafcaf4a07b17cf2d49caa5eb3cd16724c3f
Dou Tianqi <159125210+QuickFcodes@users.noreply.github.com>
2024-04-30 18:31:48 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int times;
+    while(1){
+        scanf("%d",&times);
+        for(int i;i<times;i++){
+            printf("Fuck you\n");
+        }
+    }
+}
